928d97d55ab93f182461478ba2e8f60c5f65d01d
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2025-11-06 20:27:42 +0200

    k

--- booking/settings.py
+++ booking/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking_app',
+    'auth_system',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALTTH_USER_MODEL = 'auth_system.User'
